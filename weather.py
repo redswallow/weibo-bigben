c307874e5b454e613bd9b954e5b4025ce531f83f
redswallow <redswallowjysc@gmail.com>
2013-01-10 00:36:59 +0800

    update: init api client

--- weather.py
+++ weather.py
@@ -4,18 +4,13 @@ from weibo import APIClient
 import urllib2
 import simplejson as json
 import time
+import config
 
 #config
-APP_KEY = '1037673549' # app key
-APP_SECRET = '79c0a909c1a9a48c882e6cde07fdc230' # app secret
-CALLBACK_URL = 'http://www.example.com/callback' # callback url
-
-client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
-code = 'acea5bfeb04ca56da6e4e54711e1c30f'
-r={'access_token': u'2.00WbtgYCJSyNIB378e4f5ca1vnGxAD', 'expires': 1515254019, 'expires_in': 1515254019, 'uid': u'2344884610'}
-access_token = u'2.00WbtgYCJSyNIB378e4f5ca1vnGxAD'#r.access_token 
-expires_in = 1515254019#r.expires_in 
-client.set_access_token(access_token, expires_in)
+c=config.Config()
+#init api client
+client = APIClient(app_key=c.app["app_key"], app_secret=c.app["app_secret"], redirect_uri=c.app["callback_url"])
+client.set_access_token(c.token["access_token"], c.token["expires_in"])
 
 #get_weather
 citycode='101020100'
