65124520696b1cf385b60406250cb3860007ccd7
redswallow <redswallowjysc@gmail.com>
2013-01-24 02:37:10 +0800

    GAE is read-only, add datastore

--- chat.py
+++ chat.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import time
+import time,random
 from weibo import APIClient
-import random,simplejson as json
 import config,simsimi
+from google.appengine.ext import db
+from datastore import MessageConfig
 
 import sys
 reload(sys)
@@ -18,23 +19,20 @@ client.set_access_token(c.token["access_token"], c.token["expires_in"])
 message=''
 bot = simsimi.SimSimi()
 def chatting_task():
-    print 'old since_id=%s'%c.message['since_id']
-    comments=client.get.comments__mentions(since_id=c.message['since_id'],filter_by_author=0)
+    since_id = MessageConfig().newest()
+    print 'old since_id=%s'%since_id
+    comments=client.get.comments__to_me(since_id=since_id,filter_by_author=0)
 
     for comment in comments['comments']:
-        try:
-            for words in comment['text'].split():
-                if '@' not in words:
-                    print words
-                    message=bot.chat(words)
-                    result=client.post.comments__reply(cid=comment['id'],id=comment['status']['id'],comment=message)
-                    #update since_id
-                    print 'new since_id=%d'%result['id']
-                    c.update_since_id(result['id'])
-                    print message
-            print 'cid=%d,id=%d' %(comment['id'],comment['status']['id'])
-        except:
-            pass
+        message=bot.chat(comment['text'])
+        result=client.post.comments__reply(cid=comment['id'],id=comment['status']['id'],comment=message)
+        #update since_id
+        print 'new since_id=%d'%result['id']
+        message_config = MessageConfig()
+        message_config.since_id=result['id']
+        message_config.put()
+        print message
+        #print 'cid=%d,id=%d' %(comment['id'],comment['status']['id'])
 
 if __name__=='__main__':
     chatting_task()
