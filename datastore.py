adb333a78222295507fa6f16008e93bf4e6b6566
redswallow <redswallowjysc@gmail.com>
2015-07-31 03:33:40 -0700

    Remove unused features

--- datastore.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import db
-
-class MessageConfig(db.Model):
-     since_id = db.IntegerProperty()
-
-     def newest(self):
-         return self.gql('ORDER BY since_id DESC LIMIT 1')
