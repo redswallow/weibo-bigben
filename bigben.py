655eb161e4a629fcb9018654944fb7b5af732e01
redswallow <redswallowjysc@gmail.com>
2015-07-31 03:33:59 -0700

    Convert code to be PEP8-compliant

--- bigben.py
+++ bigben.py
@@ -10,26 +10,29 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 #config
-c=config.Config()
+c = config.Config()
 #init api client
-client = APIClient(app_key=c.app["app_key"], app_secret=c.app["app_secret"], redirect_uri=c.app["callback_url"])
+client = APIClient(
+    app_key=c.app["app_key"],
+    app_secret=c.app["app_secret"],
+    redirect_uri=c.app["callback_url"])
 client.set_access_token(c.token["access_token"], c.token["expires_in"])
 
-message=''
+message = ''
 #get_hour
-sh=time.strftime("%H", time.localtime(time.time() + int(c.time['timezone']) * 60 * 60))
-h=int(sh)
+sh = time.strftime("%H", time.localtime(time.time() + int(c.time['timezone']) * 60 * 60))
+h = int(sh)
 #create message
 for i in xrange(h):
-    message=message+u"吞~"
-message+=c.message[sh]
-message=message+c.message['clock']%(sh)
+    message = message + u"吞~"
+message += c.message[sh]
+message = message + c.message['clock'] % (sh)
 #update_status
 if sh in set(c.time['pictime'].split('|')):
-    img=c.img['imgpath'] %(str(random.randint(1,int(c.img['imgnum']))))
-    f=open(img,'rb')
+    img = c.img['imgpath'] % (str(random.randint(1, int(c.img['imgnum']))))
+    f = open(img, 'rb')
     try:
-        client.upload.statuses__upload(status=message,pic=f)
+        client.upload.statuses__upload(status=message, pic=f)
     except:
         client.post.statuses__update(status=message)
     f.close()
--- config.py
+++ config.py
@@ -4,23 +4,24 @@ from ConfigParser import ConfigParser
 import os
 import codecs
 
-class Config(object): 
+
+class Config(object):
     """Config class holding all data from configuration files."""
-    def __init__(self,filename="config.ini"):
+    def __init__(self, filename="config.ini"):
         self._config = ConfigParser()
-        self.filename=filename
-        self.app={}
-        self.token={}
-        self.message={}
-        self.img={}
-        self.time={}
+        self.filename = filename
+        self.app = {}
+        self.token = {}
+        self.message = {}
+        self.img = {}
+        self.time = {}
         self.load(filename)
 
     def __parse_settings(self):
         """Parse SETTINGS section of the config file"""
-        result=self.load_config(file_name)
-        for key,value in self._config.items():
-                print " ",option,"=",self.config.get(section,option)
+        result = self.load_config(file_name)
+        for key, value in self._config.items():
+                print " ", option, "=", self.config.get(section, option)
 
     def load(self, filename):
         """Load configuration file"""
@@ -31,19 +32,19 @@ class Config(object):
         self.img = dict(self._config.items('img'))
         self.time = dict(self._config.items('time'))
 
-    def update_since_id(self,id):
-        self._config.set('message','since_id',str(id))
-        self._config.write(open(self.filename, "w")) 
+    def update_since_id(self, id):
+        self._config.set('message', 'since_id', str(id))
+        self._config.write(open(self.filename, "w"))
 
-    def load_config(self,filename):
-        try:  
-            if os.path.exists(filename):  
-                config.readfp(codecs.open(file_name, "r", "utf-8-sig"))  
-                return config  
-        except:  
-            print file_name," is not exit"  
+    def load_config(self, filename):
+        try:
+            if os.path.exists(filename):
+                config.readfp(codecs.open(file_name, "r", "utf-8-sig"))
+                return config
+        except:
+            print file_name, " is not exit"
 
-if __name__=='__main__':
-    c=Config()
+if __name__ == '__main__':
+    c = Config()
     c.update_since_id(3461259603398515)
     print c.message['since_id']
